a6f94125e5e8a817ff18d6ed5e80634154091497
Sam Thomas <sam.thomas@diageo.com>
2021-02-15 12:40:11 +0000

    Added exclusions functionality to deploy

--- redkite/deploy.py
+++ redkite/deploy.py
@@ -1,4 +1,5 @@
 import os
+from os.path import basename
 
 MODEL_NAME = 'Model.pbix'
 DELIMITER = ' -- '
@@ -8,7 +9,7 @@ def name_builder(filepath, **kwargs): # "group -- filename"
     filename = os.path.basename(filepath)
     return DELIMITER.join([group, filename])
 
-def deploy(pbi_root, workspace, dataset_params=None, credentials=None, force_refresh=False, on_report_success=None):
+def deploy(pbi_root, workspace, dataset_params=None, credentials=None, force_refresh=False, on_report_success=None, exclusions=[]):
     root, dirs, files = next(os.walk(pbi_root)) # Cycle top level folders only
     for dir in dirs:
         try: # Allow other report groups to deploy, even if others fail
@@ -18,10 +19,10 @@ def deploy(pbi_root, workspace, dataset_params=None, credentials=None, force_ref
                 print(f'! Warning: No model found in [{dir}]. Skipping folder.')
                 continue
 
-            # 2. Find report files, including in subfolders
+            # 2. Find report files, including in subfolders (but ignoring model and any file/folder exclusions)
             report_files = []
             for sub_root, sub_dirs, sub_files in os.walk(os.path.join(root, dir)):
-                new_reports = [os.path.join(sub_root, f) for f in sub_files if os.path.splitext(f)[1] == '.pbix' and f != MODEL_NAME]
+                new_reports = [os.path.join(sub_root, f) for f in sub_files if os.path.splitext(f)[1] == '.pbix' and f != MODEL_NAME and f not in exclusions and os.path.basename(sub_root) not in exclusions]
                 report_files.extend(new_reports)
 
             # 3. Deploy
