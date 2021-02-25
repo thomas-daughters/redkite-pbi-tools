159a4ff46eb7d075865b959c33422c8c0003a8bb
Sam Thomas <thomas.sam@me.com>
2021-02-25 11:47:30 +0000

    Fix issue where release names cause new Portal metadata every time (#6)
    * Add deployment routine
    
    * More portal sync to callback function
    
    * Simplify error handling
    
    * Added custom name builder function
    
    * Fixed bug in handling of kwargs
    
    * Deploy logic moved to main repo
    
    * Fix bug on deploy
    
    * Add force refresh parameter to deployment
    
    * Added exclusions functionality to deploy
    
    * Logging tweak
    
    * Fix bug on deploy return status
    
    * Add branch in name option to deploy
    
    * Fix bug on name builder
    
    * v0.1
    
    * Added optional support for config workspace
    
    * Move git tools in from public repo
    
    * Minor change to package import
    
    * Add release component to name builder
    
    * Rename private functions
    
    * Rename private functions
    
    * Fix import error
    
    * Fix import error
    
    * Fix import error
    
    * Remove .pbix from deploy name
    
    * Check whether stage is defined in Portal before trying to sync
    
    * Fix issue where release names cause new Portal metadata every time
    
    Co-authored-by: Sam Thomas <sam.thomas@diageo.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='redkite-pbi-tools',
-    version='0.2.2',
+    version='0.2.3',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Redkite DevOps tools for working with PBI',
