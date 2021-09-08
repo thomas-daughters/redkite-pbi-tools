e5457ea579e90b2e22c0cdf56fc6fe1a16cc32d6
Andy Shao <Andy.Shao1@diageo.com>
2021-09-08 15:11:26 +0100

    Adding keep_report_ids parameter

--- redkite/deploy.py
+++ redkite/deploy.py
@@ -14,7 +14,7 @@ def _name_comparator(a, b):
     b_components = b.split(DELIMITER)
     return a_components[:-1] == b_components[:-1] # Compare all except final component (which is the release)
 
-def deploy(pbi_root, workspace, dataset_params=None, credentials=None, force_refresh=None, on_report_success=None, cherry_picks=None, config_workspace=None, release=None):
+def deploy(pbi_root, workspace, dataset_params=None, credentials=None, force_refresh=None, on_report_success=None, cherry_picks=None, config_workspace=None, release=None, keep_report_ids=None):
     error = False
     root, dirs, files = next(os.walk(pbi_root)) # Cycle top level folders only
     for dir in dirs:
@@ -36,8 +36,8 @@ def deploy(pbi_root, workspace, dataset_params=None, credentials=None, force_ref
                 report_files.extend(new_reports)
 
             # 3. Deploy
-            print(f'* Deploying {len(report_files)} reports from [{dir}]')
-            workspace.deploy(dataset_file, report_files, dataset_params, credentials, force_refresh=local_force_refresh, on_report_success=on_report_success, name_builder=_name_builder, name_comparator=_name_comparator, group=dir, release=release, config_workspace=config_workspace)
+            print(f'* Deploying {len(report_files)} reports from [{dir}]')  
+            workspace.deploy(dataset_file, report_files, dataset_params, credentials, force_refresh=local_force_refresh, on_report_success=on_report_success, name_builder=_name_builder, name_comparator=_name_comparator, group=dir, release=release, config_workspace=config_workspace, keep_report_ids=keep_report_ids)
 
         except SystemExit as e:
             print(f'!! ERROR. Deployment failed for [{root}]. {e}')
